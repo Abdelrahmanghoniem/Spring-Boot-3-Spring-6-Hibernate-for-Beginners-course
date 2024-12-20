689bce214f48bddb5ed809a4bc91ff1e03af0561
Abdelrahmanghoniem <116071431+Abdelrahmanghoniem@users.noreply.github.com>
2024-12-20 22:26:10 +0200

    continue wrapping up 02

--- 02-spring-boot-spring-core/01-constructor-injection/src/main/java/com/luv2code/springcoredemo/CricketCoach.java
+++ 02-spring-boot-spring-core/01-constructor-injection/src/main/java/com/luv2code/springcoredemo/CricketCoach.java
@@ -1,4 +1,13 @@
 package com.luv2code.springcoredemo;
 
-public class CricketCoach {
+import org.springframework.stereotype.Component;
+
+@Component
+public class CricketCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout(){
+        return "practice fast bowling for 15 minutes.";
+    }
+
 }
--- 02-spring-boot-spring-core/09-java-config-bean/src/main/java/com/luv2code/springcoredemo/common/SwimCoach.java
+++ 02-spring-boot-spring-core/09-java-config-bean/src/main/java/com/luv2code/springcoredemo/common/SwimCoach.java
@@ -1,4 +1,12 @@
 package com.luv2code.springcoredemo.common;
 
-public class SwimCoach {
+public class SwimCoach implements Coach{
+
+    public SwimCoach(){
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+    @Override
+    public String getDailyWorkout() {
+        return "Swim 1000 meters as a warm up";
+    }
 }
